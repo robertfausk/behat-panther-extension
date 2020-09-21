f4057bb9e267d40a8d43726f4282b5f2679f05ac
robertfausk <robertfreigang@gmx.de>
2020-09-21 12:28:48 +0200

    Add updatePantherChromeDriver.sh to have corresponding chrome driver in travis ci available

---
+++ bin/updatePantherChromeDriver.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# ensure that symfony/panther's chromeDriver matches installed chromium version
+# this needs chromium and symfony/panther (in vendor folder) to be installed
+
+cd vendor/symfony/panther/chromedriver-bin
+
+chromiumVersion=$(chromium --product-version 2>&1;);
+googleChromeVersion=$(google-chrome --product-version 2>&1;);
+if [[ ${chromiumVersion} == *"."*"."* ]]; then
+  chromiumVersion="$( cut -d '.' -f 1 <<< "$chromiumVersion" )";
+  echo "Found chromiumVersion ${chromiumVersion}";
+  chromeDriverVersion="_${chromiumVersion}"
+elif [[ ${googleChromeVersion} == *"."*"."* ]]; then
+  googleChromeVersion="$( cut -d '.' -f 1 <<< "$googleChromeVersion" )";
+  echo "Found googleChromeVersion ${googleChromeVersion}";
+  chromeDriverVersion="_${googleChromeVersion}"
+else
+  "No google-chrome or chromium found. Using latest release..."
+  chromeDriverVersion=""
+fi
+
+chromeDriver=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE${chromeDriverVersion});
+echo "Downloading ChromeDriver version ${chromeDriver} from https://chromedriver.storage.googleapis.com/LATEST_RELEASE${chromeDriverVersion} ..."
+
+declare -a binaries=("chromedriver_linux64" "chromedriver_mac64" "chromedriver_win32")
+for name in "${binaries[@]}"
+do
+   curl -s https://chromedriver.storage.googleapis.com/${chromeDriver}/${name}.zip -O
+   unzip -q -o ${name}.zip
+   rm ${name}.zip
+   if [[ -f "chromedriver" ]]; then
+      mv chromedriver ${name}
+   fi
+done
+
+curl -s https://chromedriver.storage.googleapis.com/${chromeDriver}/notes.txt -O
+echo "Done."
+
